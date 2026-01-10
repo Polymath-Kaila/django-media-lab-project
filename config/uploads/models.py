31adc260596f5f35e3fa4498c19cd03265b114c6
Polymath-kaila <kailaevans254@gmail.com>
2026-01-10 13:30:01 -0800

    image uploads model

--- config/uploads/models.py
+++ config/uploads/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 class UploadFileModel(models.Model):
     title = models.CharField(max_length=100)
-    file = models.FileField(upload_to='files/')
+    file = models.FileField(upload_to='files/') # no validation, accepts any file type
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+
+class UploadedImageModel(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/') # validates image files
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
 
