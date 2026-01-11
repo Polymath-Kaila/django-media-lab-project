15ec08073551462380fd088703577a780ae69d0e
Polymath-kaila <kailaevans254@gmail.com>
2026-01-11 14:31:10 -0800

    changes in uploads views and urls

--- config/uploads/urls.py
+++ config/uploads/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('file', views.upload_file, name='upload_file'),
+    path('file', views.upload_file_view, name='upload_file'),
     path('image', views.upload_image, name='upload_image'),
 ]
+
+
