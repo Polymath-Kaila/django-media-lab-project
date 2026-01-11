c0a9920307c7968af982dccc966c2111d76d4782
Polymath-kaila <kailaevans254@gmail.com>
2026-01-11 14:43:06 -0800

    templates

--- config/uploads/urls.py
+++ config/uploads/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('file', views.upload_file_view, name='upload_file'),
-    path('image', views.upload_image, name='upload_image'),
+    path('file/', views.upload_file_view, name='upload_file'),
+    path('image/', views.upload_image, name='upload_image'),
 ]
 
 
